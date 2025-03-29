8b14a6ea2d28c20b193dd0aff66c26637be1ff2e
Александр Вячеславович <m-bagamaev@bk.ru>
2025-03-29 13:20:30 +0300

    Добавлены новые файлы

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "I'm alive!", 200
+def hello():
+    return "Hello, World!"
 
-if __name__ == '__master__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__master__":
+    # Получаем порт из переменной окружения PORT
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
