f33c1fca62ceec4896b0b4d1b9264a784a373b52
Александр Вячеславович <m-bagamaev@bk.ru>
2025-03-29 13:39:57 +0300

    Обновление server.py

--- server.py
+++ server.py
@@ -4,10 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def home():
+    return "Бот работает!"
 
 if __name__ == "__master__":
-    # Получаем порт из переменной окружения PORT
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000))  # Render автоматически назначает порт
+    app.run(host="0.0.0.0", port=port)
